308c85f026ae549a7adc7c6cbfe9b2d92db1d2f5
teesh3rt <ilaylevy611@gmail.com>
2025-11-15 19:06:46 +0200

    feat: add a theme, gruvbox.nvim

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,9 @@
+return {
+  { "ellisonleao/gruvbox.nvim", colorscheme = "gruvbox" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,12 +1,12 @@
 return {
-    "NeogitOrg/neogit",
-    lazy = true,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim"
-    },
-    cmd = "Neogit",
-    keys = {
-        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-    }
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
 }
